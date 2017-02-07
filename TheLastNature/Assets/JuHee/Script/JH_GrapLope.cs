dc21cf2a0a1006654f12d7305c89b64425927957
JuHee <thdwngml1223@gmail.com>
2017-02-07 15:41:56 +0900

    17.02.07 SJH Lope Add

---
+++ TheLastNature/Assets/JuHee/Script/JH_GrapLope.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JH_GrapLope : MonoBehaviour
+{
+
+    bool asdf = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Lope")
+        {
+            return;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+
+        if (other.tag == "Lope")
+        {
+            return;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+
+        if (other.tag == "Lope")
+        {
+            return;
+        }
+        if (other.tag == "Lope_handle")
+        {
+            
+                JH_lopeHandle script = other.GetComponent<JH_lopeHandle>();
+                script.F_BlopeHandle();
+            
+        }
+    }
+
+//    void OnColider
+
+
+}
---
+++ TheLastNature/Assets/JuHee/Script/JH_lopeHandle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JH_lopeHandle : Actor {
+
+    bool B_lopeHandle = false;
+    // Use this for initialization
+    void Start () {
+  
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		      //플레이어를 따라다니자
+        if (B_lopeHandle)
+        {
+            gameManager.Player.transform.position = (transform.position);
+        }
+	}
+
+    public void F_BlopeHandle()
+    {
+        B_lopeHandle = true;
+    }
+}
